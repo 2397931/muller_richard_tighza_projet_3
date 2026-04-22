92dd0aec6aa8d91c2b539daf71b5230dc7759ddd
TTylerRRichard <2496174@cmontmorency.qc.ca>
2026-04-21 22:11:47 -0400

    mod-code-shop

--- sarah_tyler_nour/Assets/ShopItemscript.cs
+++ sarah_tyler_nour/Assets/ShopItemscript.cs
@@ -6,18 +6,46 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class ShopItemscript : MonoBehaviour
 {
     public int argent;
+    
     public GameObject lantern;
+    public int lanternPrice = 650;
+
+    public GameObject sword;
+    public int swordPrice = 1200;
+
+    public GameObject shield;
+    public int shieldPrice = 800;
 
     public void OnTriggerEnter(Collider other)
     {
-        //amasser argent
-        if(argent>650)
+        // code pour la Lantern
+        if(argent >= lanternPrice)
         {
             lantern.GetComponent<XRGrabInteractable>().enabled = true;
         }
+        else
+        {
+            lantern.GetComponent<XRGrabInteractable>().enabled = false;
+        }
+
+        // code pour le sword
+        if(argent >= swordPrice)
+        {
+            sword.GetComponent<XRGrabInteractable>().enabled = true;
+        }
+        else
+        {
+            sword.GetComponent<XRGrabInteractable>().enabled = false;
+        }
+
+        // code pour le Shield
+        if(argent >= shieldPrice)
+        {
+            shield.GetComponent<XRGrabInteractable>().enabled = true;
+        }
+        else
+        {
+            shield.GetComponent<XRGrabInteractable>().enabled = false;
+        }
     }
-
-
-  
-
 }
